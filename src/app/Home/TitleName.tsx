240a5738bcbde09b60b58a640467862281b4859e
Liujosh1014 <liuchangyan1014@gmail.com>
2024-08-28 15:26:17 +0800

    seperate title into a single alphabet

--- src/app/Home/TitleName.tsx
+++ src/app/Home/TitleName.tsx
@@ -1,13 +1,59 @@
+"use client";
+
 import styles from './TitleName.module.css';
 import Button from './Button';
 import Image from 'next/image';
 import myImage from '../IMG_8760.jpg';
+import React from 'react';
 
 export default function TitleName() {
+    
+    const title = "Web Project";
+
+    const handleMouseEnter = (event: React.MouseEvent<HTMLSpanElement>) => {
+        const element = event.currentTarget;
+
+        element.classList.add(styles.isActive);
+
+        element.addEventListener('animationend', () => {
+
+            element.classList.remove(styles.isActive);
+
+        }, { once: true });
+    };
+
     return (
         <div className={styles.title}>
             <div>
-                <p>Web Project</p>
+                <div className={styles.jumpLetterContainer}>
+                    <header className={styles.jumpTitle}>
+                        {title.split('').map((letter, index) => (
+                            <React.Fragment key={index}>
+                                {letter === 'P' ? (
+                                    <>
+                                        <span className={styles.space}></span> {/* 空格 */}
+                                        <span 
+                                            className={styles.jumpTitleAlphabet}
+                                            id="special-letter"
+                                            onMouseEnter={handleMouseEnter}
+                                        >
+                                            {letter}
+                                        </span>
+                                    </>
+                                ) : (
+                                    <span
+                                        className={styles.jumpTitleAlphabet}
+                                        id={letter === 'P' ? 'special-letter' : undefined}
+                                        onMouseEnter={handleMouseEnter}
+                                    >
+                                        {letter}
+                                    </span>
+                                )}
+                            </React.Fragment>
+                        ))}
+                    </header>
+                </div>
+
                 <p className={styles.subtitle}>
                     This team website of 2024 web programming in NTOU which led by Ateto
                 </p>
