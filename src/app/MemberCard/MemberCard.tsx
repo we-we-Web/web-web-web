92f8fb2a0015556b028659d8cd9cf5a9df95a2db
01257158 <chenhongyu056@gmail.com>
2024-09-07 23:18:09 +0800

    新增成員卡片

---
+++ src/app/MemberCard/MemberCard.tsx
@@ -0,0 +1,157 @@
+import styles from './MemberCard.module.css';
+import Image from 'next/image';
+
+export default function MemberCard(){
+
+    return (
+        <>
+            <div className={styles.introTitle}>
+                <h2>Member Introduction</h2>
+            </div>
+
+            <div className={styles.team}>
+
+                <div className={styles.card}>
+                    <div className={styles.cardImg}>
+                        <Image src= "https://github.com/Ateto1204.png" alt="蘇東毅" width={370} height={400}/>
+                    </div>
+                    <div className={styles.cardContent}>
+                        <div className={styles.ecardContentHeader}>
+                            <h2 className={styles.cardContentHeading}>蘇東毅
+                                <a href="https://Ateto1204.github.io/" target="_blank">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-link-45deg" viewBox="0 0 16 16">
+                                        <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
+                                        <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
+                                    </svg>
+                                </a>
+                            </h2>    
+                            <p className={styles.cardContentSubheading}>Taiwan</p>
+                        </div>
+                        
+                        
+                    </div>
+                </div>
+            
+                <div className={styles.card}>
+                    <div className={styles.cardImg}>
+                        <Image src="https://github.com/01257158.png" alt="陳宏瑜" width={370} height={400}/>
+                    </div>
+                    <div className={styles.cardContent}>
+                        <div className={styles.cardContentHeader}>
+                            <h2 className={styles.cardContentHeading}>陳宏瑜
+                                <a href="https://01257158.github.io/" target="_blank">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-link-45deg" viewBox="0 0 16 16">
+                                        <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
+                                        <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
+                                    </svg>
+                                </a>
+                            </h2>
+                            <p className={styles.cardContentSubheading}>Taiwan</p>
+                        </div>
+                        
+                    </div>
+                </div>
+            
+                <div className={styles.card}>
+                    <div className={styles.cardImg}>
+                        <Image src="https://github.com/tyuLD.png" alt="王鈞宇" width={370} height={400}/>
+                    </div>
+                    <div className={styles.cardContent}>
+                        <div className={styles.cardContentHeader}>
+                            <h2 className={styles.cardContentHeading}>王鈞宇
+                                <a href="https://tyuLD.github.io/" target="_blank">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-link-45deg" viewBox="0 0 16 16">
+                                        <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
+                                        <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
+                                    </svg>
+                                </a>
+                            </h2>
+                            <p className={styles.cardContentSubheading}>Taiwan</p>
+                        </div>
+                        
+                    </div>
+                </div>
+            
+                <div className={styles.card}>
+                    <div className={styles.cardImg}>
+                        <Image src="https://github.com/Liujosh1014.png" alt="劉長諺" width={370} height={400}/>
+                    </div>
+                    <div className={styles.cardContent}>
+                        <div className={styles.cardContentHeader}>
+                            <h2 className={styles.cardContentHeading}>劉長諺
+                                <a href="https://Liujosh1014.github.io/" target="_blank">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-link-45deg" viewBox="0 0 16 16">
+                                        <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
+                                        <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
+                                    </svg>
+                                </a>
+                            </h2>
+                            <p className={styles.cardContentSubheading}>Taiwan</p>
+                        </div>
+                        
+                    </div>
+                </div>
+            
+                <div className={styles.card}>
+                    <div className={styles.cardImg}>
+                        <Image src="https://github.com/Ilenol.png" alt="林彥均" width={370} height={400}/>
+                    </div>
+                    <div className={styles.cardContent}>
+                        <div className={styles.cardContentHeader}>
+                            <h2 className={styles.cardContentHeading}>林彥均
+                                <a href="https://Ilenol.github.io/" target="_blank">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-link-45deg" viewBox="0 0 16 16">
+                                        <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
+                                        <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
+                                    </svg>
+                                </a>
+                            </h2>
+                            <p className={styles.cardContentSubheading}>Taiwan</p>
+                        </div>
+                        
+                    </div>
+                </div>
+            
+                <div className={styles.card}>
+                    <div className={styles.cardImg}>
+                        <Image src="https://github.com/alanlu01.png" alt="呂翰昇" width={370} height={400}/>
+                    </div>
+                    <div className={styles.cardContent}>
+                        <div className={styles.cardContentHeader}>
+                            <h2 className={styles.cardContentHeading}>呂翰昇
+                                <a href="https://alanlu01.github.io/" target="_blank">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-link-45deg" viewBox="0 0 16 16">
+                                        <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
+                                        <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
+                                    </svg>
+                                </a>
+                            </h2>
+                            <p className={styles.cardContentSubheading}>Taiwan</p>
+                        </div>
+                        
+                    </div>
+                </div>
+            
+                <div className={styles.card}>
+                    <div className={styles.cardImg}>
+                        <Image src="https://github.com/cjc094.png" alt="陳婕熙" width={370} height={400}/>
+                    </div>
+                    <div className={styles.cardContent}>
+                        <div className={styles.cardContentHeader}>
+                            <h2 className={styles.cardContentHeading}>陳婕熙
+                                <a href="https://cjc094.github.io/" target="_blank">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-link-45deg" viewBox="0 0 16 16">
+                                        <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
+                                        <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
+                                    </svg>
+                                </a>
+                            </h2>
+                            <p className={styles.cardContentSubheading}>Taiwan</p>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Home from './Home/Home'
 import TitleName from './Home/TitleName';
+import MemberCard from "./MemberCard/MemberCard";
 
 export default function page() {
   return (
     <div>
       <Home/>
       <TitleName/>
+      <MemberCard/>
     </div>
   );
 }
