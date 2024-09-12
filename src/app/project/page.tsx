80f5e3de267d4e4ed44c233129c202278de4fe17
cjc094 <01257157@email.ntou.edu.tw>
2024-09-12 17:37:29 +0800

    division add

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,8 +1,9 @@
 import styles from './page.module.css';
 import Tag from './skillTag/SkillTag';
+import Image from 'next/image';
 
 export default function Project() {
-    
+
     const frontendSkills = [
         'React (Vite)',
     ];
@@ -44,6 +45,39 @@ export default function Project() {
                             <li key={index}>{skill}</li>
                         ))}
                     </ul>
+                    <div className={styles['team-members']}>
+                        <div className={styles['team-member']}>
+                            <Image src='https://github.com/Ateto1204.png' alt='Ateto' width={140} height={150} style={{ borderRadius: '50%' }} />
+                            <ul>
+                                <p>蘇東毅</p>
+                                <li>後端</li>
+                                <li>資料綁定</li>
+                                <li>畫面跳轉</li>
+                                <li>UI/UX 優化</li>
+                            </ul>
+                        </div>
+                        <div className={styles['team-member']}>
+                            <Image src='https://github.com/tyuLD.png' alt='王鈞宇' width={140} height={150} />
+                            <ul>
+                                <p>王鈞宇</p>
+                                <li>抽籤動畫</li>
+                            </ul>
+                        </div>
+                        <div className={styles['team-member']}>
+                            <Image src='https://github.com/Ilenol.png' alt='林彥君' width={140} height={150}  />
+                            <ul>
+                                <p>林彥君</p>
+                                <li>匯出 Excel 抽籤結果</li>
+                            </ul>
+                        </div>
+                        <div className={styles['team-member']}>
+                            <Image src='https://github.com/cjc094.png' alt='jc' width={140} height={150}  />
+                            <ul>
+                                <p>陳婕熙</p>
+                                <li>登入畫面</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
