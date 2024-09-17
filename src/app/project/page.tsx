8fb01f01cb0ca5714ac6ab47545ca28ba4b92c80
tyu <225jimmy@gmail.com>
2024-09-17 17:28:46 +0800

    Refactor skill tag

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -2,7 +2,7 @@ import styles from './page.module.css';
 import Tag from './skillTag/SkillTag';
 
 export default function Project() {
-    
+    const colors = ['#B7B7BD', '#D4B8B4', '#ADAAA5', '#CCD2CC', '#C09D9B', '#DBD4C6'];
     const frontendSkills = [
         'React (Vite)',
     ];
@@ -22,10 +22,10 @@ export default function Project() {
                 <h1>海大資工新生直屬抽籤全端系統</h1>
                 <div className={styles['title-skills']}>
                     {frontendSkills.map((skill, index) => (
-                        <Tag key={index} skill={skill} />
+                        <Tag key={index} skill={skill} color={'#DBD4C6'}/>
                     ))}
                     {backendSkills.map((skill, index) => (
-                        <Tag key={index} skill={skill} />
+                        <Tag key={index} skill={skill} color={colors[index]}/>
                     ))}
                 </div>
             </div>
