8097ed3c7e7b8a34919f9213050a384af06143ea
Ateto1202 <tonysu1204@gmail.com>
2024-09-18 10:45:36 +0800

    Abstraction project view assign member fragment

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -2,18 +2,71 @@ import styles from './page.module.css';
 import Tag from './skillTag/SkillTag';
 import Image from 'next/image';
 
+interface AssignMemberProps {
+    imageSrc: string,
+    name: string, 
+    tasks: string[], 
+    theme: string, 
+}
+
+function AssignMember({ imageSrc, name, tasks, theme }: AssignMemberProps) {
+    return (
+        <div className={styles['assign-member']}>
+            <Image
+                src={imageSrc}
+                alt={name}
+                width={140}
+                height={150}
+                style={{ border: `3px solid ${theme}` }}
+            />
+            <div className={styles['whiteboard']} style={{ backgroundColor: theme }}>
+                <p style={{ textAlign: 'center' }}>
+                    {name}
+                </p>
+                { tasks.map((task, index) => (
+                    <li key={index}>{task}</li>
+                )) }
+            </div>
+        </div>
+    );
+}
+
 export default function Project() {
     const colors = ['#B7B7BD', '#D4B8B4', '#ADAAA5', '#CCD2CC', '#C09D9B', '#DBD4C6'];
-    const frontendSkills = [
-        'React (Vite)',
-    ];
-
+    const frontendSkills = ['React (Vite)'];
     const backendSkills = [
         'Go (Gin)',
         'Node.js (Express.js)',
         'PostgreSQL',
         'MongoDB',
         'Redis',
+    ];
+
+    const teamMembers = [
+        {
+            imageSrc: 'https://github.com/Ateto1204.png',
+            name: '蘇東毅',
+            tasks: ['後端', '資料綁定', '畫面跳轉', 'UI/UX 優化'],
+            theme: '#C09D9B',
+        },
+        {
+            imageSrc: 'https://github.com/tyuLD.png',
+            name: '王鈞宇',
+            tasks: ['抽籤動畫'],
+            theme: '#B7B7BD',
+        },
+        {
+            imageSrc: 'https://github.com/Ilenol.png',
+            name: '林彥君',
+            tasks: ['匯出 Excel 抽籤結果'],
+            theme: '#C09D9B',
+        },
+        {
+            imageSrc: 'https://github.com/cjc094.png',
+            name: '陳婕熙',
+            tasks: ['登入畫面'],
+            theme: '#CCD2CC',
+        },
     ];
 
     return (
@@ -23,10 +76,10 @@ export default function Project() {
                 <h1>海大資工新生直屬抽籤全端系統</h1>
                 <div className={styles['title-skills']}>
                     {frontendSkills.map((skill, index) => (
-                        <Tag key={index} skill={skill} color={'#DBD4C6'}/>
+                        <Tag key={index} skill={skill} color={'#DBD4C6'} />
                     ))}
                     {backendSkills.map((skill, index) => (
-                        <Tag key={index} skill={skill} color={colors[index]}/>
+                        <Tag key={index} skill={skill} color={colors[index]} />
                     ))}
                 </div>
             </div>
@@ -45,65 +98,17 @@ export default function Project() {
                             <li key={index}>{skill}</li>
                         ))}
                     </ul>
-
-                    <div className={styles['team-members']}>
-                        <div className={styles['team-member']}>
-                            <Image
-                                src='https://github.com/Ateto1204.png'
-                                alt='Ateto'
-                                width={140}
-                                height={150}
-                                style={{ border: '3px solid #C09D9B' }}
-                            />
-                            <div className={styles['whiteboard']} style={{ border: '2px solid #e0e0e0',backgroundColor:'#C09D9B' }}>
-                                <p style={{ textAlign: 'center' }}>蘇東毅</p>
-                                <li>後端</li>
-                                <li>資料綁定</li>
-                                <li>畫面跳轉</li>
-                                <li>UI/UX 優化</li>
-                            </div>
-                        </div>
-
-                        <div className={styles['team-member']}>
-                            <Image
-                                src='https://github.com/tyuLD.png'
-                                alt='王鈞宇'
-                                width={140}
-                                height={150}
-                                style={{ border: '3px solid #B7B7BD' }}
-                            />
-                            <div className={styles['whiteboard']} style={{ border: '2px solid #e0e0e0' ,backgroundColor:'#B7B7BD'}}>
-                                <p style={{ textAlign: 'center' }}>王鈞宇</p>
-                                <li>抽籤動畫</li>
-                            </div>
-                        </div>
-
-                        <div className={styles['team-member']}>
-                            <Image src='https://github.com/Ilenol.png'
-                                alt='林彥君'
-                                width={140}
-                                height={150}
-                                style={{ border: '3px solid #C09D9B' }}
-                            />
-                            <div className={styles['whiteboard']} style={{ border: '2px solid #e0e0e0' ,backgroundColor: '#C09D9B'}}>
-                                <p style={{ textAlign: 'center' }}>林彥君</p>
-                                <li>匯出 Excel 抽籤結果</li>
-                            </div>
-                        </div>
-
-                        <div className={styles['team-member']}>
-                            <Image src='https://github.com/cjc094.png'
-                                alt='jc'
-                                width={140}
-                                height={150}
-                                style={{ border: '3px solid #CCD2CC' }}
-                            />
-                            <div className={styles['whiteboard']} style={{ border: '2px solid #e0e0e0', backgroundColor: '#CCD2CC'}}>
-                                <p style={{ textAlign: 'center' }}>陳婕熙</p>
-                                <li>登入畫面</li>
-                            </div>
-                        </div>
-                    </div>
+                </div>
+                <div className={styles['assign-member-set']}>
+                    {teamMembers.map((member, index) => (
+                        <AssignMember
+                            key={index}
+                            imageSrc={member.imageSrc}
+                            name={member.name}
+                            tasks={member.tasks}
+                            theme={member.theme}
+                        />
+                    ))}
                 </div>
             </div>
         </div>
